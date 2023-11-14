6442fcaca7e310f9e0cabd9e7a817a82e319c664
Jose-L17 <100440883+Jose-L17@users.noreply.github.com>
2023-11-14 17:49:30 -0600

    Update app-routing.module.ts #25

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MediaComponent } from './media/media.component';
 import { DesviacionComponent } from './desviacion/desviacion.component';
+import { CorrelationComponent } from './correlation/correlation.component';
 
 const routes: Routes = [
-  { path: 'Media', component: DesviacionComponent }
+  { path: 'Media', component: DesviacionComponent },
+    { path: 'Regrsion', component: CorrelationComponent },
 ];
 
 
