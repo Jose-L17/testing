db6aa6b0da6007b488ccbee966037472a65ac279
Jose-L17 <100440883+Jose-L17@users.noreply.github.com>
2023-11-14 00:15:50 -0600

    Update app-routing.module.ts #18

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MediaComponent } from './media/media.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Media', component: MediaComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
