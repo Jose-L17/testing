8753fb6fb5532d8277340d9d2c36a6b3cc5898fe
Jose-L17 <100440883+Jose-L17@users.noreply.github.com>
2023-11-14 12:19:57 -0600

    Update desviacion.component.spec.ts #20

--- src/app/desviacion/desviacion.component.spec.ts
+++ src/app/desviacion/desviacion.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { of } from 'rxjs';
 import { DesviacionComponent } from './desviacion.component';
 import { HoursService } from '../services/data.service';
@@ -14,11 +17,10 @@ describe('DesviacionComponent', () => {
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [ DesviacionComponent ],
-      imports: [ HttpClientTestingModule ],
-      providers: [ HoursService, SizeService ]
-    })
-    .compileComponents();
+      declarations: [DesviacionComponent],
+      imports: [HttpClientTestingModule],
+      providers: [HoursService, SizeService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DesviacionComponent);
     component = fixture.componentInstance;
@@ -31,13 +33,17 @@ describe('DesviacionComponent', () => {
   });
 
   it('should return mean = 60.32 with the data Hours', () => {
-    component.horas = { data: [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2] };
+    component.horas = {
+      data: [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2],
+    };
     const result = component.getMedia(...component.horas.data);
     expect(result).toBe(60.32);
   });
 
   it('should return mean = 550.6 with the data Size', () => {
-    component.size = { data: [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503] };
+    component.size = {
+      data: [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503],
+    };
     const result = component.getMedia(...component.size.data);
     expect(result).toBe(550.6);
   });
@@ -55,7 +61,9 @@ describe('DesviacionComponent', () => {
   });
 
   it('should return standard deviation = 62.26 with data Hours', () => {
-    component.horas = { data: [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2] };
+    component.horas = {
+      data: [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2],
+    };
     const media = component.getMedia(...component.horas.data);
     const result = component.getDesviacion(component.horas.data, media);
     expect(result).toBe(62.26);
@@ -72,8 +80,152 @@ describe('DesviacionComponent', () => {
   });
 
   it('should handle data for getHours and getSize during ngOnInit', async () => {
-    const hoursData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-    const sizeData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+    const hoursData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const sizeData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    spyOn(component, 'getHours').and.returnValue(Promise.resolve());
+    spyOn(component, 'getSize').and.returnValue(Promise.resolve());
+
+    spyOn(component.hoursService, 'getHours').and.returnValue(of(hoursData));
+    spyOn(component.sizeService, 'getSize').and.returnValue(of(sizeData));
+
+    await component.ngOnInit();
+    component.horas = hoursData;
+    component.size = sizeData;
+    expect(component.horas).toEqual(hoursData);
+    expect(component.size).toEqual(sizeData);
+  });
+
+  it('should return data for getHours', async () => {
+    const hoursData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    spyOn(component.hoursService, 'getHours').and.returnValue(of(hoursData));
+
+    await component.getHours();
+
+    expect(component.horas).toEqual(hoursData);
+  });
+
+  it('should return data for getSize', async () => {
+    const sizeData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    spyOn(component.sizeService, 'getSize').and.returnValue(of(sizeData));
+
+    await component.getSize();
+
+    expect(component.size).toEqual(sizeData);
+  });
+
+  // Prueba para cubrir this.size.data && Array.isArray(this.size.data)
+  it('should return data for getSize', async () => {
+    const sizeData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    spyOn(component.sizeService, 'getSize').and.returnValue(of(sizeData));
+
+    await component.getSize();
+
+    expect(component.size).toEqual(sizeData);
+  });
+
+  // Prueba para cubrir this.horas && this.horas.data && Array.isArray(this.horas.data)
+  it('should return data for getHours', async () => {
+    const hoursData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    spyOn(component.hoursService, 'getHours').and.returnValue(of(hoursData));
+
+    await component.getHours();
+
+    expect(component.horas).toEqual(hoursData);
+  });
+
+  it('should calculate media_hours if horas.data is valid', () => {
+    const horasData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    component.horas = { data: horasData };
+
+    spyOn(component, 'getMedia').and.returnValue(5.5);
+
+    component.obtenerMediaHours();
+
+    expect(component.getMedia).toHaveBeenCalledWith(...horasData);
+
+    expect(component.media).toBeDefined();
+  });
+
+  it('should calculate media_size if size.data is valid', () => {
+    const sizeData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    component.size = { data: sizeData };
+
+    spyOn(component, 'getMedia').and.returnValue(5.5);
+
+    component.obtenerMediaSize();
+
+    expect(component.getMedia).toHaveBeenCalledWith(...sizeData);
+
+    expect(component.media).toBeDefined();
+  });
+
+  it('should calculate desviacion_hours if horas.data is valid', () => {
+    const horasData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    component.horas = { data: horasData };
+
+    spyOn(component, 'getMedia').and.returnValue(5.5);
+    spyOn(component, 'getDesviacion').and.returnValue(3.03);
+
+    component.calcularDesviacionHours();
+
+    expect(component.getMedia).toHaveBeenCalledWith(...horasData);
+    expect(component.getDesviacion).toHaveBeenCalledWith(horasData, 5.5);
+
+    expect(component.desviacion_hours).toBeDefined();
+  });
+
+  it('should calculate desviacion_size if size.data is valid', () => {
+    const sizeData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    component.size = { data: sizeData };
+
+    spyOn(component, 'getMedia').and.returnValue(5.5);
+    spyOn(component, 'getDesviacion').and.returnValue(3.03);
+
+    component.calcularDesviacionSize();
+
+    expect(component.getMedia).toHaveBeenCalledWith(...sizeData);
+    expect(component.getDesviacion).toHaveBeenCalledWith(sizeData, 5.5);
+
+    expect(component.desviacion_size).toBeDefined();
+  });
+
+  it('should call getHours and getSize during ngOnInit', async () => {
+    spyOn(component, 'getHours').and.returnValue(Promise.resolve());
+    spyOn(component, 'getSize').and.returnValue(Promise.resolve());
+
+    await component.ngOnInit();
+
+    expect(component.getHours).toHaveBeenCalled();
+    expect(component.getSize).toHaveBeenCalled();
+  });
+
+  it('should return 0 when numbers array is empty', () => {
+    const result = component.getMedia();
+    expect(result).toBe(0);
+  });
+
+  it('should call getHours and getSize during ngOnInit', async () => {
+    spyOn(component, 'getHours').and.returnValue(Promise.resolve());
+    spyOn(component, 'getSize').and.returnValue(Promise.resolve());
+
+    await component.ngOnInit();
+
+    expect(component.getHours).toHaveBeenCalled();
+    expect(component.getSize).toHaveBeenCalled();
+  });
+
+  it('should handle data for getHours and getSize during ngOnInit', async () => {
+    const hoursData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const sizeData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     spyOn(component, 'getHours').and.returnValue(Promise.resolve());
     spyOn(component, 'getSize').and.returnValue(Promise.resolve());
@@ -108,28 +260,6 @@ describe('DesviacionComponent', () => {
     expect(component.size).toEqual(sizeData);
   });
 
-  // Prueba para cubrir this.size.data && Array.isArray(this.size.data)
-  it('should return data for getSize', async () => {
-    const sizeData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-
-    spyOn(component.sizeService, 'getSize').and.returnValue(of(sizeData));
-
-    await component.getSize();
-
-    expect(component.size).toEqual(sizeData);
-  });
-
-  // Prueba para cubrir this.horas && this.horas.data && Array.isArray(this.horas.data)
-  it('should return data for getHours', async () => {
-    const hoursData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-
-    spyOn(component.hoursService, 'getHours').and.returnValue(of(hoursData));
-
-    await component.getHours();
-
-    expect(component.horas).toEqual(hoursData);
-  });
-
   it('should calculate media_hours if horas.data is valid', () => {
     const horasData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
@@ -141,7 +271,7 @@ describe('DesviacionComponent', () => {
 
     expect(component.getMedia).toHaveBeenCalledWith(...horasData);
 
-    expect(component.media_hours).toBeDefined();
+    expect(component.media).toBeDefined();
   });
 
   it('should calculate media_size if size.data is valid', () => {
@@ -155,54 +285,28 @@ describe('DesviacionComponent', () => {
 
     expect(component.getMedia).toHaveBeenCalledWith(...sizeData);
 
-    expect(component.media_size).toBeDefined();
-  });
-
-  it('should calculate desviacion_hours if horas.data is valid', () => {
-    const horasData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-
-    component.horas = { data: horasData };
-
-    spyOn(component, 'getMedia').and.returnValue(5.5);
-    spyOn(component, 'getDesviacion').and.returnValue(3.03);
-
-    component.calcularDesviacionHours();
-
-    expect(component.getMedia).toHaveBeenCalledWith(...horasData);
-    expect(component.getDesviacion).toHaveBeenCalledWith(horasData, 5.5);
-
-    expect(component.desviacion_hours).toBeDefined();
-  });
-
-  it('should calculate desviacion_size if size.data is valid', () => {
-    const sizeData = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-
-    component.size = { data: sizeData };
-
-    spyOn(component, 'getMedia').and.returnValue(5.5);
-    spyOn(component, 'getDesviacion').and.returnValue(3.03);
-
-    component.calcularDesviacionSize();
-
-    expect(component.getMedia).toHaveBeenCalledWith(...sizeData);
-    expect(component.getDesviacion).toHaveBeenCalledWith(sizeData, 5.5);
-
-    expect(component.desviacion_size).toBeDefined();
-  });
-
-  it('should call getHours and getSize during ngOnInit', async () => {
-    spyOn(component, 'getHours').and.returnValue(Promise.resolve());
-    spyOn(component, 'getSize').and.returnValue(Promise.resolve());
-
-    await component.ngOnInit();
-
-    expect(component.getHours).toHaveBeenCalled();
-    expect(component.getSize).toHaveBeenCalled();
+    expect(component.media).toBeDefined();
   });
 
   it('should return 0 when numbers array is empty', () => {
     const result = component.getMedia();
     expect(result).toBe(0);
   });
+
+
+  it('should return 0 when array is empty', () => {
+    let datos =  { data: [1, 2, 3, 4 , 5 ,6 ,7 ,8 ,9, 10]};
+    const result = component.calcularMedia(datos);
+    expect(result).toBe(5.5);
+  });
+
+  //Prueba para probar calcularDesviacion
+  it('should return 0 when array is empty', () => {
+    let datos =  { data: [1, 2, 3, 4 , 5 ,6 ,7 ,8 ,9, 10]};
+    const result = component.calcularDesviacion(datos);
+    expect(result).toBe(3.03);
+  });
+
+
 
 });
