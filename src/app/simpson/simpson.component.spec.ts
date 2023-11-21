3609395f0a39613b49fe3a650e5c25fcc0d9a715
Jose-L17 <100440883+Jose-L17@users.noreply.github.com>
2023-11-21 09:43:57 -0600

    Update simpson.component.spec.ts #27

--- src/app/simpson/simpson.component.spec.ts
+++ src/app/simpson/simpson.component.spec.ts
@@ -1,119 +1,121 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { simpson } from './simpson.component';
-import { SimpsonComponent } from './simpson.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { By } from '@angular/platform-browser';
+import { fx_1eX } from './f(x)=1ex';
+import { fx_2x } from './f(x)=2x';
+import { fx_XxX } from './f(x)=XxX';
+import { simpsonMethodForT } from '../distribucionT/t';
+import { Component } from '@angular/core';
 
-describe('media test suite', () => {
+@Component({
+  selector: 'app-simpson',
+  templateUrl: './simpson.component.html',
+  styleUrls: ['./simpson.component.css'],
+})
+export class SimpsonComponent {
+  public f = '';
+  public x0 = 0;
+  public x1 = 0;
+  public num_seg = 0;
+  public dof = 0;
+  public resultado = 0;
+  public error = 0.0;
 
-  let component: SimpsonComponent;
-  let fixture: ComponentFixture<SimpsonComponent>;
+  ActualizarF(event: Event): void {
 
-  beforeEach(async()  => {
+    this.f = (event.target as HTMLInputElement).value;
+  }
 
-    await TestBed.configureTestingModule({
-      imports: [HttpClientModule, HttpClientTestingModule],
-      declarations: [SimpsonComponent],
-    }).compileComponents();
+  ActualizarX0(event: Event): void {
+    this.x0 = parseFloat((event.target as HTMLInputElement).value);
+  }
 
-    component = new SimpsonComponent();
-    fixture = TestBed.createComponent(SimpsonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  ActualizarX1(event: Event): void {
+    this.x1 = parseFloat((event.target as HTMLInputElement).value);
+  }
 
-  });
+  ActualizarNumSeg(event: Event): void {
+    this.num_seg = parseFloat((event.target as HTMLInputElement).value);
+  }
 
-  it('Should return p = 16 if f(x) = 2x, x0=0, x1=4, num_seg=4, dof=0', () => {
-    const resultado = simpson('2x', 0, 4, 4, 0, 0.00001);
-    expect(resultado).toBe(16);
-  });
+  ActualizarDof(event: Event): void {
+    this.dof = parseFloat((event.target as HTMLInputElement).value);
+  }
 
-  it('Should return p=0.33333 if f(x) = x*x, x0=0, x1=1, num_seg=4, dof =0', () => {
-    const resultado = simpson('x*x', 0, 1, 4, 0, 0.00001);
-    expect(resultado).toBe(0.33333);
-  });
+  ActualizarError(event: Event): void {
+    this.error = parseFloat((event.target as HTMLInputElement).value);
+  }
 
-  it('Should return p=1 if f(x) = 1/x, x0=1, x1=4, num_seg=6, dof=0', () => {
-    const resultado = simpson('1/x', 1, 1, 6, 0, 0.00001);
-    expect(resultado).toBe(1);
-  });
+  Funcion_Simpson() {
+    this.resultado = simpson(this.f, this.x0, this.x1, this.num_seg, this.dof, this.error);
+    return this.resultado;
+  }
 
-  it('Should return p = 0.35006 if X = 1.1, num_seg = 10 and Dof = 9', () => {
-    const result = simpson('', 0, 1.1, 10, 9, 0.00001);
-    expect(result).toBe(0.35006);
-  });
+}
 
+export function simpson(
+  f: string,
+  x0: number,
+  x1: number,
+  num_seg: number,
+  dof: number,
+  error: number,
+) {
+  let E =error; // Tolerancia de error
+  let x = 0; // Variable para almacenar la posición actual
+  let xC; // Valor de la función en x
+  let y; // Valor del término y en la regla de Simpson
+  let area_seg; // Área de cada segmento
+  let P = 0; // Suma acumulada de áreas de segmentos
+  let PF; // Resultado final de la aproximación de la integral
+  let contador = 0; // Contador de iteraciones
+  const arrP = []; // Arreglo para almacenar las sumas parciales
+  let margenE = 1; // Margen de error
 
-  it('Should return p=1 if f(x) = 1/x, x0=1, x1=4, num_seg=6, dof=0', () => {
-    const resultado = simpson('5/x', 1, 1, 6, 0, 0.00001);
-    expect(resultado).toBe(1);
-    });
+  if (dof == 0) {
+    do {
+      margenE = 0; // Reinicia el margen de error en cada iteración
+      let W = (x1 - x0) / num_seg; // Ancho de cada segmento
+      let W3 = W / 3; // Tercio del ancho del segmento
+      x = x0; // Inicializa la posición en el límite inferior del intervalo
 
+      for (let i = 0; i <= num_seg; i++) {
+        if (f.toLowerCase() == '2x') {
+          xC = fx_2x(x);
+        } else if (f.toLowerCase() == 'x*x') {
+          xC = fx_XxX(x);
+        } else if (f.toLowerCase() == '1/x') {
+          xC = fx_1eX(x);
+        } else {
+          xC = fx_2x(x); // Valor predeterminado si f no coincide con ninguno
+        }
 
-    it('Actualiza la propiedad f correctamente', () => {
-      const mockEvent = { target: { value: 'test' } } as any as Event;
-      component.ActualizarF(mockEvent);
-      expect(component.f).toBe('test');
-    });
+        if (i == 0 || i == num_seg) {
+          y = xC * 1; // Primer y último término
+        } else {
+          y = i % 2 === 0 ? xC * 2 : xC * 4; // Términos pares e impares
+        }
 
-    it('Actualiza la propiedad x0 correctamente', () => {
-      const mockEvent = { target: { value: '42' } } as any as Event;
-      component.ActualizarX0(mockEvent);
-      expect(component.x0).toBe(42);
-    });
+        area_seg = y * W3; // Área del segmento actual
+        P += area_seg; // Acumula el área en P
+        x += W; // Avanza a la siguiente posición
+      }
 
-    it('Actualiza la propiedad x1 correctamente', () => {
-      const mockEvent = { target: { value: '10.5' } } as any as Event;
-      component.ActualizarX1(mockEvent);
-      expect(component.x1).toBe(10.5);
-    });
+      arrP.push(P); // Almacena la suma parcial en el arreglo
+      num_seg = num_seg * 2; // Duplica el número de segmentos
+      contador += 1; // Incrementa el contador de iteraciones
 
-    it('Actualiza la propiedad num_seg correctamente', () => {
-      const mockEvent = { target: { value: '5' } } as any as Event;
-      component.ActualizarNumSeg(mockEvent);
-      expect(component.num_seg).toBe(5);
-    });
+      if (contador > 0) {
+        margenE = arrP[0] - arrP[contador]; // Calcula el margen de error
+      }
 
-    it('Actualiza la propiedad dof correctamente', () => {
-      const mockEvent = { target: { value: '3' } } as any as Event;
-      component.ActualizarDof(mockEvent);
-      expect(component.dof).toBe(3);
-    });
+      PF = parseFloat(P.toFixed(5)); // Redondea PF a 5 decimales
+      P = 0; // Reinicia P para la próxima iteración
+    } while (margenE > E || contador < 2); // Continúa hasta que se cumpla el criterio de parada
 
-    it('Actualiza la propiedad error correctamente', () => {
-      const mockEvent = { target: { value: '0.00001' } } as any as Event;
-      component.ActualizarError(mockEvent);
-      expect(component.error).toBe(0.00001);
-    });
+    if (PF == 0) {
+      PF = 1; // Si el resultado es 0, se establece en 1 (evita divisiones por 0)
+    }
 
-    it('Calcula correctamente el resultado de la función Simpson', () => {
-      component.f = ' x*x';
-      component.x0 = 0;
-      component.x1 = 1;
-      component.num_seg = 10;
-      component.dof = 0;
-      component.error = 0.00001;
-
-      component.Funcion_Simpson();
-
-      expect(component.resultado).toBeCloseTo(1);
-    });
-
-    it("Revisa que se manden a llamar los métodos de ActualizarF, ActualizarX0, ActualizarX1, ActualizarNumSeg y ActualizarDof al presionar el boton ObtenerResultado", () => {
-
-        let botonObtenerResultado = fixture.debugElement.query(By.css('.ObtenerResultado'));
-
-        botonObtenerResultado.triggerEventHandler('click', null);
-
-        expect(component.f).toBeDefined();
-        expect(component.x0).toBeDefined();
-        expect(component.x1).toBeDefined();
-        expect(component.num_seg).toBeDefined();
-        expect(component.dof).toBeDefined();
-        expect(component.error).toBeDefined();
-        expect(component.resultado).toBeDefined();
-    });
-
-});
-
+    return PF; // Devuelve el resultado final de la aproximación de la integral
+  } else {
+    return simpsonMethodForT(dof, num_seg, x1);
+  }
+}
